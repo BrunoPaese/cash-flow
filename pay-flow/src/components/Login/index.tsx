cd5009a48dc17895b99564b62455387c3b46d1bf
Bruno Paese <brunoviniciuspaese@gmail.com>
2025-12-16 22:37:57 -0300

    feat(feature/login): renderiza logo

--- pay-flow/src/components/Login/index.tsx
+++ pay-flow/src/components/Login/index.tsx
@@ -1,13 +1,19 @@
 import Button from "../Button";
 import Input from "../Input";
 import { Page } from "./style";
+import logoDark from "../../assets/logo_dark.png";
 
 function Login() {
   return (
     <Page theme="dark">
       <div className="login-card">
-        <img src="/logo.png" alt="Logo do sistema" className="login-logo" />
-        <h1>Entrar</h1>
+        <img
+          src={logoDark}
+          width={300}
+          height={300}
+          alt="Logo do sistema"
+          className="login-logo"
+        />
         <Input label="teste" type="email" />
         <Input label="teste" type="password" />
         <Button theme="dark">teste</Button>
