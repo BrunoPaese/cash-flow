12f8650c90c9dad6d97057692a3903777d52904f
Bruno <brunoviniciuspaese@gmail.com>
2025-12-19 22:51:08 -0300

    feat(login): implementa traducoes

--- pay-flow/src/components/Input/index.tsx
+++ pay-flow/src/components/Input/index.tsx
@@ -2,14 +2,14 @@ import { Container, StyledInput } from "./style";
 import type { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  placeholder: string;
   theme?: "light" | "dark";
 }
 
-function Input({ label, theme = "light", ...props }: InputProps) {
+function Input({ placeholder, theme = "light", ...props }: InputProps) {
   return (
     <Container>
-      <StyledInput theme={theme} placeholder={label} {...props} />
+      <StyledInput theme={theme} placeholder={placeholder} {...props} />
     </Container>
   );
 }
